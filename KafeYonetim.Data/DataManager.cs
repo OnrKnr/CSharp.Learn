99968e329cd2d72e4de30994169f0537f2b4e99b
Orhan Aygün <orhan.aygun@outlook.com>
2017-11-21 11:32:34 +0300

    test

--- KafeYonetim.Data/DataManager.cs
+++ KafeYonetim.Data/DataManager.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 
 namespace KafeYonetim.Data
 {
+    //
     public class DataManager
     {
         private static string connStr = "Data Source=DESKTOP-S3O5AOR;Initial Catalog=KafeYonetim;Integrated Security=True";
